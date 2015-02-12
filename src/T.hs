ed37bdb12bd62728fdd93441ba7797a9e9f2d293
theNerd247 <harvey.coder247@gmail.com>
2015-02-12 18:48:21 -0500

    finished showTask formatting

--- src/T.hs
+++ src/T.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 colwidth = 20
 linechar = '-'
 colchar = '|'
+minutesperline = 30
 
 -- a task is made of a start date, end date, description, and a priority
 -- taskTimes should be of at least length 2
@@ -20,7 +21,7 @@ data TaskTime d t = TaskTime
 
 -- creates the display string for all of the TaskTime Pairs for a given 
 -- Task. Returns the list of display strings
-showTask :: (Show dt, Show t, Show p) => (Task dt t p String) -> [String]
+showTask :: (Integral dt, Integral t, Show dt, Show t, Show p, Show d) => (Task dt t p d) -> [String]
 showTask (Task {taskTimes=tts,taskPrio=p,taskDesc=d}) = pTasks (tts,p,d)
 	where 
 		pTasks ([],_,_) = [""]
@@ -30,23 +31,30 @@ showTask (Task {taskTimes=tts,taskPrio=p,taskDesc=d}) = pTasks (tts,p,d)
 		-- the print layout for a single task 
 		pTask t1 t2 p d = (pTimeLine t1 t2 p)  -- print the line with priority and time info
 			++ "\n" ++ (showDesc t1 t2 d)  -- print the description (formatted)
-			++ "\n" ++ lineFilled -- put a blank line at the end
+			++ lineFilled -- put a blank line at the end
 
 		pTimeLine t1 t2 p = insertAt 7 (show $ taskTimeTime t1)
 			$ insertAt (colwidth-6) (show $ taskTimeTime t2)
 			$ insertAt 3 (show p) lineFilled
 
 -- formats the description of a task given the start and end times and the description
-showDesc :: (Show ta) => ta -> ta -> String -> String
-showDesc t1 t2 d = (L.intercalate "\n" $ putInLine <$> formatLines) ++ "\n" ++ blines 
+showDesc :: (Integral ta, Integral tb, Show ta, Show tb, Show d) => (TaskTime ta tb) -> (TaskTime ta tb) -> d -> String
+showDesc t1 t2 d = truncLines $ (L.intercalate "\n" $ putInLine <$> formatLines) ++ blines 
 	where
-		formatLines = splitEvery colwidth d
-		blines = nBlankLines $ (timesToLines t2 t1) - (length formatLines) 
+		formatLines = splitEvery colwidth (show d)
+		blines = ('\n':) . nBlankLines $ n 
 		putInLine = (flip $ insertAt 1) lineBlank
+		n = fromIntegral (timesToLines t1 t2) - (length formatLines) 
+		truncLines = unlines . (\s -> take (sn s) s) . lines 
+		sn = (n+) . length
 
 -- converts a time difference into a number of lines
-timesToLines :: a -> a -> Int
-timesToLines t1 t2 = 5
+timesToLines :: (Integral dt, Integral t, Integral y) => (TaskTime dt t) -> (TaskTime dt t) -> y
+timesToLines (TaskTime dt1 t1) (TaskTime dt2 t2) = linesPerMinute $ (fromIntegral deldt)+(fromIntegral delt)
+	where 
+		deldt = dt2 - dt1
+		delt = t2 - t1
+		linesPerMinute = round . (/minutesperline) . toRational
 
 -- convenience functions to draw lines with column markers
 -- a : |--...--| line
@@ -55,7 +63,10 @@ lineFilled = line linechar
 -- a : |  ...  | line
 lineBlank = line ' '
 
-nBlankLines = L.intercalate "\n" . (flip L.replicate) lineBlank
+nBlankLines :: Int -> String
+nBlankLines x
+	| x <= 0 = ""
+	| otherwise = L.intercalate "\n" $ L.replicate x lineBlank
 
 -- a : |cc...cc| line
 line c = colchar : (replicate colwidth c) ++ (colchar:[])
@@ -78,6 +89,11 @@ splitEvery _ [] = []
 splitEvery n xs = (fst sp) : splitEvery n (snd sp)
 	where sp = L.splitAt n xs
 
-tst :: (Show dt, Show t, Show p) => (Task dt t p String) -> IO ()
-tst = putStrLn . f 
-	where f = (flip (!!) 0) . showTask
+ta = TaskTime 12 10
+tb = TaskTime 182 10
+t = Task [ta,tb,ta,tb] 'a' "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,"
+tst :: (Integral dt, Integral t, Show dt, Show t, Show p) => (Task dt t p String) -> IO ()
+tst = f . showTask
+	where 
+		f [] = return ()
+		f (s:ss) = putStrLn s >>= (\s -> f ss)
