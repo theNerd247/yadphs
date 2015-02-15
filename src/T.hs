4cda1d678728329af0caf9020d09802bd3ae2711
theNerd247 <harvey.coder247@gmail.com>
2015-02-14 22:11:06 -0500

    added new class structure and basic task and time types

--- src/T.hs
+++ src/T.hs
@@ -2,71 +2,85 @@
 
 import qualified Data.List as L
 import Control.Applicative
+import Data.Time.LocalTime as LT
+import Data.Time.Calendar as C
 
 colwidth = 20
 linechar = '-'
 colchar = '|'
 minutesperline = 30
+pTimeLength = colwidth-6
+pPrioLength = 3
+sndTimePos = colwidth-12
+
+class (Show t) => Timeable t where
+	-- must be able to compute the # of minutes
+	toMinutes :: (Integral y) => t -> y
 
 -- TODO: refine these type classes to set up the structure for printing a task
-class (Timeable t, Functor tt) => Taskable tt t p d where
-	showTask :: (tt t p d) -> String
-
-class Timeable t where
-	date :: (Show y, Integral y) => t -> y
-	time :: (Show y, Integral y) => t -> y
-	timesTolines :: (Integral y) => t -> t -> y
-	formatDesc :: t -> t -> String -> String
+class Taskable t where
+	showTask :: (Timeable tt, Show tt, Show p, Show d) => t tt p d -> String
 
 -- a task is made of a start date, end date, description, and a priority
 -- taskTimes should be of at least length 2
 data Task tt p d = Task
-	{taskTimes :: [tt]
+	{taskTime :: tt
 	,taskPrio :: p 
 	,taskDesc :: d
 	}	deriving (Eq, Ord, Show)
 
-data TaskTime d t = TaskTime
-	{taskTimeDate :: d
-	,taskTimeTime :: t
-	} deriving (Eq, Ord, Show)
+{-f :: (Timeable t, Show d) => t -> d -> String-}
+{-f t = showDesc (diffTime <*> timeNull $ t) . show -}
 
--- creates the display string for all of the TaskTime Pairs for a given 
--- Task. Returns the list of display strings
-showTask ::  => (Task dt t p d) -> [String]
-showTask (Task {taskTimes=tts,taskPrio=p,taskDesc=d}) = pTasks (tts,p,d)
-	where 
-		pTasks ([],_,_) = [""]
-		pTasks ([x],_,_) = [""]
-		pTasks ((t1:t2:tt),p,d) = (pTask t1 t2 p d) : pTasks (tt,p,d)
+instance Taskable Task where
+	showTask (Task {taskTime=tm,taskPrio=p,taskDesc=d}) = 
+			(pt tm p) -- print the line with priority and time info
+			++ ddblk -- print the description (formatted)
+			++ lf -- put a blank line at the end
+		where 
+			pt t = pTimeLine (show t) . show
+			dd t = showDesc (toMinutes t) . show
+			dblk = dd tm d
+			ddblk -- insert a new line for the description block if it's not empty
+				| (length dblk > 0) = '\n':dblk
+				| otherwise = ""
+			lf -- a filled line for the bottom of a task (doesn't show if there is no description block)
+				| (length dblk > 0) = lineFilled
+				| otherwise = ""
 
-		-- the print layout for a single task 
-		pTask t1 t2 p d = (pTimeLine t1 t2 p)  -- print the line with priority and time info
-			++ "\n" ++ (showDesc t1 t2 d)  -- print the description (formatted)
-			++ lineFilled -- put a blank line at the end
+data OnceTime d t = OnceTime d t deriving (Eq,Ord)
 
-		pTimeLine t1 t2 p = insertAt 7 (show $ taskTimeTime t1)
-			$ insertAt (colwidth-6) (show $ taskTimeTime t2)
-			$ insertAt 3 (show p) lineFilled
+instance (Show d, Show t) => Show (OnceTime d t) where
+	show (OnceTime d t) = pSecondTime t
 
--- formats the description of a task given the start and end times and the description
-showDesc :: (Integral ta, Integral tb, Show ta, Show tb, Show d) => (TaskTime ta tb) -> (TaskTime ta tb) -> d -> String
-showDesc t1 t2 d = truncLines $ (L.intercalate "\n" $ putInLine <$> formatLines) ++ blines 
+instance (Show d, Show t) => Timeable (OnceTime d t) where
+	toMinutes (OnceTime _ _) = 0
+
+-- takes the time format string and the priority for a task and creates a filled
+-- line 
+pTimeLine ::  String -> String -> String
+pTimeLine t p = insertAt 7 (take pTimeLength t)
+	$ insertAt 3 p lineFilled
+
+-- left and right spots on the time line
+-- (helper functions for showing Timeable types)
+pFirstTime t = insertAt 0 (take 5 $ show t) ptLine
+pSecondTime t = insertAt sndTimePos (take 5 $ show t) ptLine
+ptLine = replicate pTimeLength linechar
+
+-- format the description given the time difference and the string
+showDesc :: (Real t) => t -> String -> String
+showDesc t d = truncLines $ (L.intercalate "\n" $ putInLine <$> formatLines) ++ blines 
 	where
-		formatLines = splitEvery colwidth (show d)
+		formatLines = splitEvery colwidth d
 		blines = ('\n':) . nBlankLines $ n 
-		putInLine = (flip $ insertAt 1) lineBlank
-		n = fromIntegral (timesToLines t1 t2) - (length formatLines) 
+		n = (timesToLines t) - (length formatLines)
 		truncLines = unlines . (\s -> take (sn s) s) . lines 
 		sn = (n+) . length
 
--- converts a time difference into a number of lines
-timesToLines :: (Integral dt, Integral t, Integral y) => (TaskTime dt t) -> (TaskTime dt t) -> y
-timesToLines (TaskTime dt1 t1) (TaskTime dt2 t2) = linesPerMinute $ (fromIntegral deldt)+(fromIntegral delt)
-	where 
-		deldt = dt2 - dt1
-		delt = t2 - t1
-		linesPerMinute = round . (/minutesperline) . toRational
+-- converts a time difference (in minutes) into a number of lines
+timesToLines :: (Real n) => n -> Int
+timesToLines = round . (/minutesperline) . toRational
 
 -- convenience functions to draw lines with column markers
 -- a : |--...--| line
@@ -80,7 +94,9 @@ nBlankLines x
 	| x <= 0 = ""
 	| otherwise = L.intercalate "\n" $ L.replicate x lineBlank
 
+putInLine = (flip $ insertAt 1) lineBlank
 -- a : |cc...cc| line
+--
 line c = colchar : (replicate colwidth c) ++ (colchar:[])
 
 -- generic insert at function (used above to insert text into a line 
@@ -101,11 +117,7 @@ splitEvery _ [] = []
 splitEvery n xs = (fst sp) : splitEvery n (snd sp)
 	where sp = L.splitAt n xs
 
-ta = TaskTime 12 10
-tb = TaskTime 182 10
-t = Task [ta,tb,ta,tb] 'a' "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,"
-tst :: (Integral dt, Integral t, Show dt, Show t, Show p) => (Task dt t p String) -> IO ()
-tst = f . showTask
-	where 
-		f [] = return ()
-		f (s:ss) = putStrLn s >>= (\s -> f ss)
+dt = fromGregorian 2015 02 10
+tt = TimeOfDay 12 30 0
+tm = OnceTime dt tt
+tsk = Task tm 'A' "blarg nad fa;ksdjf ;ajsd"
